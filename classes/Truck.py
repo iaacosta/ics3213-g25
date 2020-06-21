0d8aeb909a8cd5c6685c42ab7f625233bf276e87
Nacho <iaacosta@uc.cl>
2020-06-21 17:00:29 -0400

    improvement: adds logs to route time computing

--- classes/Truck.py
+++ classes/Truck.py
@@ -3,7 +3,7 @@ from .Route import Route
 
 class Truck:
     def __init__(self, _id):
-        self.id = int(_id)
+        self.id = int(_id) + 1
         self.routes = {}
 
     def add_point(self, route_id, point):
@@ -15,7 +15,9 @@ class Truck:
     def compute_route_times(self, service):
         times = {}
 
+        print(f'Computing times for truck {self.id}')
         for route in list(self.routes.values()):
+            print(f'  Computing times for route {route.id}')
             times[route.id] = route.compute_times(service)
 
         return times
